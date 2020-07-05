db928e436327d3a5c8b3a78bedf9d53e8ff039c4
Heverton <heverton.gomes2018@gmail.com>
2020-07-04 21:41:45 -0300

    Adiação de backend e layout

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from  './styles.module.css'
 
 const Footer = () => {
     return (
-        <div className='bg-gray-700 p-4'>
+        <div className= 'bg-gray-700 p-4 '>
             <div className='container mx-auto text-center font-bold text-white'>
                 Projeto desenvolvido por: Heverton Gomes
                 {' '} / <a className='hover:underline' target="_blank" href='https://www.linkedin.com/in/heverton-gomes-8221181a5/'>Linkedin</a>
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,18 +1,90 @@
-import React from 'react'
-import Link from 'next/link'
+import React, {useState} from 'react'
+import PageTitle from '../components/PageTitle'
 
 const Pesquisa = () => {
+
+    const [form, setForm] = useState({
+        Nome: '',
+        Email: '',
+        Whatsapp: '',
+        Nota: 0
+    })
+
+    const [sucess, setSucess] = useState(false)
+    const [retorno, setRetorno] = useState({})
+
+    const notas = [0, 1, 2, 3, 4, 5]
+    const save = async() => {
+        
+        try{
+            const response = await fetch('api/save', {
+                method: 'POST',
+                body: JSON.stringify(form)
+            })
+            const data = await response.json()
+            setSucess(true)
+            setRetorno(data)
+        }catch(err){
+
+        }
+    }
+
+    const onChage = evt => {
+        const value = evt.target.value
+        const key = evt.target.name
+        setForm(old => ({
+            ...old,
+            [key]: value
+        }))
+    }
+
     return (
         <div className='py-6'>
-         
+            <PageTitle title='Pesquisa'/>
             <h1 className='text-center font-bold my-4 text-2xl'>Críticas e Sugestões</h1>
             <p className='text-center mb-6'>
                 O restaurante X sempre busca por atender melhor seus clientes.<br/> Por isso, estamos sempre abe
             </p>
+            {!sucess && <div className='w-1/5 mx-auto'>
+                <label className='font-bold'>Nome:</label>
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Nome' onChange={onChage} name='Nome' value={form.Nome}/>
+                <label className='font-bold'>Email:</label>
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="email" placeholder='Email'  onChange={onChage} name='Email' value={form.Email}/>
+                <label className='font-bold'>Whatsapp:</label>
+                <input className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Whatsapp'  onChange={onChage} name='Whatsapp' value={form.Whatsapp}/>
+                <label className='font-bold'>Dê um nota:</label>
+                <div className='flex py-6'>
+                    { notas.map(nota => {
+                        return (
+                            <label className='block w-1/6 text-center'>
+                                {nota} <br/>
+                                <input type="radio" name='Nota' value={nota} onChange={onChage}/>
+                            </label>)
+                    })
+                }
+                </div>
+                <button className = 'bg-green-400 px-4 py-2 font-bold rounded-lg shadow-lg hover:shadow'
+                 onClick={save}>Salvar 
+                </button>
+            </div>}
+        {sucess && 
             <div className='w-1/5 mx-auto'>
-                <label className='font-bold'>Seu nome:</label>
-                <input className='p-4 block shadow bg-blue-100 my-2 rounded' type="text" name="" id=""/>
+                <p className='mb-6 text-center bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert"'>Obrigado pela sugestão ou crítica</p>
+                {
+                   retorno.showCoupom && <div className='text-center border p-4 mb-4'>
+                       Seu cupom: <br />
+                       <span className='font-bold text-2xl'>{retorno.Cupom}</span>
+                   </div>
+                }
+                {
+                   retorno.showCoupom && <div className='text-center border p-4 mb-4'>
+                       <span className='font-bold block mb-2'>{retorno.Promo}</span>
+                        <br/>
+                        <span className='italic'>Tire um print ou foto e apresente na hora pagamento</span>
+                   </div>
+                }
             </div>
+        }
         </div>
     )
 }
